6ab80d650266832426043309d42d173d739f52ce
kyuhyeong <kkhgud1@naver.com>
2022-10-03 20:30:55 +0900

    사용자가 입력한 숫자 검증

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,15 +1,22 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 public class BaseballGame {
 
     private ArrayList<Integer> comNumber;
+    private ArrayList<Integer> userNumber;
     private final int MAX_SIZE = 3;
     private final int START_NUM = 1;
     private final int END_NUM = 9;
+    private int ball = 0;
+    private int strike = 0;
 
 
     public ArrayList<Integer> baseballNumber(){
@@ -18,6 +25,35 @@ public class BaseballGame {
         }
 
         return comNumber;
+
+    }
+
+    public void userInputNumber(){
+        System.out.println("숫자를 입력해 주세요");
+        String intputNum = Console.readLine();
+        makeUserNumber(intputNum);
+        checkUserInput();
+
+    }
+    public ArrayList<Integer> makeUserNumber(String inNum){
+        String[] inArr = inNum.split("");
+        for(int i = 0; i<inArr.length; i++){
+            userNumber.add(Integer.parseInt(inArr[i]));
+        }
+        return userNumber;
+    }
+
+    public void checkUserInput(){
+        if(userNumber.size() != MAX_SIZE){
+            throw new IllegalArgumentException("3자리 숫자를 입력해주세요");
+        }
+        if(userNumber.contains(0)){
+            throw new IllegalArgumentException("0을 제외한 숫자를 입력해주세요");
+        }
+        Set<Integer> tmpSet = new HashSet<>(userNumber);
+        if(userNumber.size() != tmpSet.size()){
+            throw new IllegalArgumentException("중복된 숫자가 있습니다. 다시 입력해주세요");
+        }
 
     }
 
